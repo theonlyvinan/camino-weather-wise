cd5b0fc3773ea1fd3b9c5232e338b1bce22cba01
theonlyvinan <theonlyvinan@gmail.com>
2025-07-15 22:37:06 -0700

    Update App.tsx
    added capacitor status bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import { StatusBar } from '@capacitor/status-bar';
+StatusBar.setOverlaysWebView({ overlay: false });
 
 const queryClient = new QueryClient();
 
